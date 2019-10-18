8967de1ebf014d23ebd2e0c15a4d209642a68576
Claire Collins <ccc.22@dartmouth.edu>
2019-10-18 14:58:28 -0400

    editied to complete module step 2

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -11,7 +11,23 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <webpage.h>
 
 int main(void){
-	printf("hello");
+	char seed[40]="https://thayer.github.io/engs50/Labs";
+	char *url=seed;
+	webpage_t *w=webpage_new(seed, 0, NULL);
+	if(!webpage_fetch(w)){
+		exit(EXIT_FAILURE);
+	}
+	char *content=webpage_getHTML(w); 
+	printf("%s", content);
+	if (IsInternalURL(url)){
+		printf("internal");
+	}
+	else {
+		printf("external");
+	}
+	webpage_delete(w);
+	exit(EXIT_SUCCESS);
 }
