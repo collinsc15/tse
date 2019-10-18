4dc8860ac262577d7bc4536de19956f4c6b435ef
Claire Collins <ccc.22@dartmouth.edu>
2019-10-18 15:15:28 -0400

    added notes

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,4 +1,4 @@
-/* crawler.c --- 
+/* crawler.c --- crawls a website
  * 
  * 
  * Author: Claire C. Collins
@@ -14,20 +14,20 @@
 #include <webpage.h>
 
 int main(void){
-	char seed[40]="https://thayer.github.io/engs50/Labs";
-	char *url=seed;
-	webpage_t *w=webpage_new(seed, 0, NULL);
-	if(!webpage_fetch(w)){
+	char seed[40]="https://thayer.github.io/engs50/";   //url to be pointed at
+	char *url=seed;                                    //point at url
+	webpage_t *w=webpage_new(seed, 0, NULL);    //creates webpage from url
+	if(!webpage_fetch(w)){    //fetches web page if false exits
 		exit(EXIT_FAILURE);
 	}
-	char *content=webpage_getHTML(w); 
-	printf("%s", content);
-	if (IsInternalURL(url)){
+	char *content=webpage_getHTML(w); //gets html of website
+	printf("%s", content);       //prints content of website
+	if (IsInternalURL(url)){    //scans if website is internal
 		printf("internal");
 	}
 	else {
 		printf("external");
 	}
-	webpage_delete(w);
+	webpage_delete(w);         //deletes webpage
 	exit(EXIT_SUCCESS);
 }
