46ed8cff12c3a9db0159d0ee4b7d834d37f7d5aa
Vlado42 <vlado.vojdanovski.22@dartmouth.edu>
2019-10-19 22:28:30 -0400

    fixed queue

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -16,6 +16,15 @@
 #include <queue.h> 
 #include <hash.h>
 
+// A simple helper function that checks whether our hash has a URL
+int hashContainsURL(void* currPage, const void* searchAddress){
+	char* currPageURL = webpage_getURL((webpage_t*)currPage);
+	if (*currPageURL != *(char *)searchAddress){
+		return false;
+	}
+	return true;
+}
+
 int main(void){
 	char seed[40]="https://thayer.github.io/engs50/";   //url to be pointed at
 	char *url=seed;//point at url
@@ -39,22 +48,28 @@ int main(void){
 
 	// initiating search of all internal hyperlinks
 	int pos = 0;
-	char *currPage; // charr array that stores the URl we are looking at
+	char *currPageURL; // charr array that stores the URl we are looking at
 
 
 	// while there are URLs to be read
-	while((pos = webpage_getNextURL(w, pos, &currPage)) > 0 ) {
-		if (IsInternalURL(currPage)){    //if the url is internal
+	while((pos = webpage_getNextURL(w, pos, &currPageURL)) > 0 ) {
+		if (IsInternalURL(currPageURL)){    //if the url is internal
+			//	if hsearch(hashOfTables,hashContainsURL
+			webpage_t* currPage=webpage_new(currPageURL, 0, NULL); //our webpage
 			qput(qOfWebPages, currPage); //put it in our queue
+		  free(currPageURL);
 		}
 	}
-	free(currPage);
+	free(currPageURL);
+
 
 	// while our queue is non-empty
 	void* currGet;
 	while ((currGet = qget(qOfWebPages)) != NULL){
-		printf("%s\n",(char*)currGet); //print what it returns
-		free(currGet);
+    char*	poppedURL = webpage_getURL((webpage_t*)currGet);
+		printf("%s\n",poppedURL); //print the URL of the popped page
+		free(poppedURL); // free the url memory			
+		free(currGet); //  free the entire webpage
 	}
 	
 	qclose(qOfWebPages);
