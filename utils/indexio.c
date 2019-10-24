44078801555c3dbb71f85f3755ad00f46b7bc0e2
Claire Collins <ccc.22@dartmouth.edu>
2019-10-24 17:17:16 -0400

    started it

---
+++ utils/indexio.c
@@ -0,0 +1,21 @@
+/* indexio.c --- 
+ * 
+ * 
+ * Author: Claire C. Collins
+ * Created: Thu Oct 24 16:59:51 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+
+int32_t indexsave(index_t index, char *indexnm, char *dirname){
+
+
+}
+
+index_t indexload(char *indexnm, char *dirname){
+
+
+}
