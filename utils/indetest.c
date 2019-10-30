144047aee2537b09fa42c2c967ba92abbb44765a
Vlado42 <vlado.vojdanovski.22@dartmouth.edu>
2019-10-30 14:03:02 -0400

    indextesT'

---
+++ utils/indetest.c
@@ -0,0 +1,28 @@
+/* indetest.c --- 
+ * 
+ * 
+ * Author: Vlado Vojdanovski
+ * Created: Wed Oct 30 13:42:56 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <unistd.h>                                                                                                                     
+#include <stdlib.h>                                                                                                                     
+#include <dirent.h>                                                                                                                     
+#include <stdio.h>                                                                                                                      
+#include <webpage.h>                                                                                                                    
+#include <queue.h>                                                                                                                      
+#include <hash.h>                                                                                                                       
+#include <string.h>                                                                                                                     
+#include <sys/types.h>                                                                                                                  
+#include <sys/stat.h> 
+#include <webpage.h> 
+#include <indexio.h>
+
+void main(void)
+{
+  hashtable_t *w= indexload("BillClinton", "indexes");  
+  indexsave(w,"BillClinton2", "indexes");
+}    
