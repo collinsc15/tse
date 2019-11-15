6c88671d6d4488dd64109463d0894b498de8e720
Vlado42 <vlado.vojdanovski.22@dartmouth.edu>
2019-11-14 19:14:58 -0500

    fixed it for you

---
+++ utils/lqueue.c
@@ -0,0 +1,78 @@
+/* lqueue.c --- 
+ * 
+ * 
+ * Author: Vlado Vojdanovski
+ * Created: Thu Nov 14 18:21:25 2019 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <queue.h>
+
+typedef locked_queue
+{
+	queue_t* queue;
+	pthread_mutex_t lock;
+} lockedQ;
+	
+void lqopen(void)
+{
+	lockedQ* newLockedQ = malloc(sizeof(lockedQ));
+	newLockedQ -> queue = qopen();
+	pthread_mutex_init(newLockedQ->lock, NULL); 
+}
+
+void lqclose(lockedQ *qp)
+{
+	pthread_mutex_lock(qp -> lock);
+	qclose(qp -> queue);
+	pthread_mutex_unlock(qp -> lock);
+	pthread_mutex_destroy(qp -> lock);
+	free(qp);
+}
+
+int32_t lqput(lockedQ *qp, void *elementp)
+{
+	pthread_mutex_lock(qp -> lock);
+	qput(qp -> queue, elementp);
+	pthread_mutex_unlock(qp -> unlock);
+}
+
+void *lqget(lockedQ *qp)
+{
+	pthread_mutex_lock(qp -> lock);
+	void* element = qpget(qp -> queue);
+	pthread_mutex_unlock(qp -> unlock);
+	return element;
+}
+
+void lqapply(lockedQ *qp, void(*fn)(void* elementp))
+{
+	pthread_mutex_lock(qp -> lock);
+	qapply(qp->queue, void(*fn)(void* elementp));
+	pthread_mutex_unlock(qp ->unlock);
+}
+
+void* lqsearch(lockedQ *qp, bool(*searchfn)(void* elementp. const void* keyp), const void* skeyp)
+{
+	pthread_mutex_lock(qp->lock);
+	void* element = qsearch(qp -> queue, bool(*searchfn)(void* elementp. const void* keyp), const void* skeyp);
+	pthread_mutex_unlock(qp -> unlock);
+	return element;
+}
+
+//void* lqremove(locked *qp, bool(*searchfn)(void* elementp, const void* keyp),
+//							const void* skeyp)
+//{
+//	pthread_mutex_lock(qp -> lock);
+//	void* element = qremove(qp->queue,bool(*searchfn)(void* elementp, const void* keyp),
+//													const void* skeyp);
+//	pthread_mutex_unlock(qp -> lock);
+//	return element;
+//}
+
