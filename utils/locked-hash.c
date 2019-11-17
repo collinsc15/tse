5b50c359147146ef41c8fd8df151b8de5ebb3ee3
Claire Collins <ccc.22@dartmouth.edu>
2019-11-16 23:12:10 -0500

    fixed some syntactical errors

--- utils/locked-hash.c
+++ utils/locked-hash.c
@@ -10,16 +10,20 @@
  */
 #include <hash.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 typedef struct lockedhash_t{
 	hashtable_t *hash;
 	pthread_mutex_t lock;
-};
+} lockedhash_t;
 
-lockedhash_t lhopen(int32_t hsize){
+lockedhash_t *lhopen(int32_t hsize){
 	lockedhash_t *lhtp= (lockedhash_t *)malloc(sizeof(lockedhash_t));
-	hashtable_t lhtp->hash=hopen(hsize);
-	pthread_mutex_init(&lhtp->lock,NULL);
+	if (!(lhtp)){
+		return NULL;
+	}
+	lhtp->hash=hopen(hsize);
+	pthread_mutex_init(&(lhtp->lock),NULL);
 	return lhtp;
 }
 
@@ -36,6 +40,7 @@ int32_t lhput(lockedhash_t *lhtp, void *ep, const char *key, int keylen){
 	pthread_mutex_lock(&(lhtp->lock));
 	hput(lhtp->hash, ep, key, keylen);
 	pthread_mutex_unlock(&(lhtp->lock));
+	return 0;
 }
 
 void lhapply(lockedhash_t *lhtp, void (*fn)(void* ep)){
@@ -53,7 +58,7 @@ void *lhsearch(lockedhash_t *lhtp,
 	return ele;
 }
 
-void *lhremove(hashtable_t *lhtp,                                                                                                  
+void *lhremove(lockedhash_t *lhtp,                                                                                                  
 							bool (*searchfn)(void* elementp, const void* searchkeyp),                                                                
 							const char *key,                                                                                                         
 							int32_t keylen){
