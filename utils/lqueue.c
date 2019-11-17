225ebcf885a1176e6502c08d54326e2605400096
Claire Collins <ccc.22@dartmouth.edu>
2019-11-17 17:41:08 -0500

    fixed some errors and lowered sleep time

--- utils/lqueue.c
+++ utils/lqueue.c
@@ -37,8 +37,9 @@ void* lqopen(void)
 void lqclose(void *qp)
 {
 	pthread_mutex_lock(&(((lockedQ*)qp) -> lock));
-	sleep(10);
-	//qclose((((lockedQ*)qp)->queue));
+	
+	qclose((((lockedQ*)qp)->queue));
+	sleep(2);
 	pthread_mutex_unlock(&(((lockedQ*)qp) -> lock));
 	pthread_mutex_destroy(&(((lockedQ*)qp) -> lock));
 	free(((lockedQ*)qp));
@@ -47,12 +48,12 @@ void lqclose(void *qp)
 int32_t lqput(void *qp, void *elementp)
 {
 	pthread_mutex_lock(&(((lockedQ*)qp) -> lock));
-	sleep(10);
 	if(!qput((((lockedQ*)qp) -> queue), elementp))
 		{
 			pthread_mutex_unlock(&(((lockedQ*)qp) -> lock));
 			return 1;
 		}
+	sleep(2);
 	pthread_mutex_unlock(&(((lockedQ*)qp) -> lock));
 	return 0;
 }
@@ -60,8 +61,8 @@ int32_t lqput(void *qp, void *elementp)
 void *lqget(void *qp)
 {
 	pthread_mutex_lock(&(((lockedQ*)qp) -> lock));
-	sleep(10);
 	void* element = qget(&(((lockedQ*)qp) -> queue));
+	sleep(2);
 	pthread_mutex_unlock(&(((lockedQ*)qp) -> lock));
 	return element;
 }
@@ -69,16 +70,18 @@ void *lqget(void *qp)
 void lqapply(void *qp, void(*fn)(void* elementp))
 {
 	pthread_mutex_lock(&(((lockedQ*)qp) -> lock));
-	sleep(10);
+	
 	qapply((((lockedQ*)qp)->queue), fn);
+	sleep(2);
 	pthread_mutex_unlock(&(((lockedQ*)qp) ->lock));
 }
 
 void* lqsearch(void *qp, bool(*searchfn)(void* elementp, const void* keyp), const void* skeyp)
 {
-	pthread_mutex_lock(&(((lockedQ*)qp)->lock));
-	sleep(10);
+	pthread_mutex_lock(&(((lockedQ*)qp) -> lock));
 	void* element = qsearch((((lockedQ*)qp) -> queue),searchfn, skeyp);
+	sleep(2);
+	
 	pthread_mutex_unlock(&(((lockedQ*)qp) -> lock));
 	return element;
 }
--- utils/lqueue.h
+++ utils/lqueue.h
@@ -9,21 +9,21 @@
 typedef void lqueue_t;		
 
 /* create an empty queue */
-lqueue_t* qopen(void);        
+lqueue_t* lqopen(void);        
 
 /* deallocate a queue, frees everything in it */
-void qclose(lqueue_t *qp);   
+void lqclose(lqueue_t *qp);   
 
 /* put element at the end of the queue
  * returns 0 is successful; nonzero otherwise 
  */
-int32_t qput(lqueue_t *qp, void *elementp); 
+int32_t lqput(lqueue_t *qp, void *elementp); 
 
 /* get the first first element from queue, removing it from the queue */
-void* qget(lqueue_t *qp);
+void* lqget(lqueue_t *qp);
 
 /* apply a function to every element of the queue */
-void qapply(lqueue_t *qp, void (*fn)(void* elementp));
+void lqapply(lqueue_t *qp, void (*fn)(void* elementp));
 
 /* search a queue using a supplied boolean function
  * skeyp -- a key to search for
@@ -34,7 +34,7 @@ void qapply(lqueue_t *qp, void (*fn)(void* elementp));
  *          -- returns TRUE or FALSE as defined in bool.h
  * returns a pointer to an element, or NULL if not found
  */
-void* qsearch(lqueue_t *qp, 
+void* lqsearch(lqueue_t *qp, 
 							bool (*searchfn)(void* elementp,const void* keyp),
 							const void* skeyp);
 
