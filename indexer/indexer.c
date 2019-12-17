7db64ad956cbbca641aadad7803a0db78ac44af8
Vlado42 <52867647+Vlado42@users.noreply.github.com>
2019-12-17 16:19:33 -0600

    Update indexer.c

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -1,12 +1,10 @@
 /* indexer.c --- 
-1;5202;0c * 
  * 
  * Author: Claire C. Collins
  * Created: Sun Oct 27 18:39:02 2019 (-0400)
- * Version: 
- * 
- * Description: 
- * 
+ * usage: indexer <pagedir><indexnm>
+ * where pagedir is the directory where we have the saved pages,
+ * and indexnm is the name we would like to save our index as
  */
 #include <stdio.h>
 #include <webpage.h>
@@ -35,8 +33,6 @@ void add(void *docq){
 
 void adderFunc(void* hashWord) {
 	qapply(((word_t*)hashWord)->docs,add);
-				 //total_count += ((word_t*)hashWord) -> occurences; // add the datA to the totalCount
-	//printf("%s:%d\n",((word_t*)hashWord)->name,((word_t*)hashWord)->occurences)
 }
 
 void docSprintf(void *doc){ 
@@ -52,7 +48,7 @@ void indexSprintf(void* hashWord){
 
 
 void closeThoseDamnQueues(void* hashWord) {
-  qclose(((word_t*)hashWord)->docs); // add the datA to the totalCount
+  qclose(((word_t*)hashWord)->docs); // add the data to the totalCount
 }
 
 void addObj(void* hashWord){
@@ -61,8 +57,6 @@ void addObj(void* hashWord){
 
 bool hashContainsWord(void* hashWord, const void* searchWord){
 	word_t *obj =(word_t*)hashWord;
-	//strcpy(h,obj->name);
-	//printf("objectname:%s\n",obj->name);
 	if (!strcmp(obj->name, (char*)searchWord)) {
 		return true;
 	}
@@ -71,8 +65,6 @@ bool hashContainsWord(void* hashWord, const void* searchWord){
 
 bool queueContainsDoc(void* doc, const void* searchWord){
 	doc_t *obj =(doc_t*)doc;
-	//strcpy(h,obj->name);
-	//printf("objectname:%s\n",obj->name);
 	if (!strcmp(obj->name, (char*)searchWord)) {
 		return true;
 	}
@@ -80,7 +72,7 @@ bool queueContainsDoc(void* doc, const void* searchWord){
 }
 
 void NormalizeWord(char word[]){                                               
-  char *p = word;                                                                //check that word has more than three characters
+  char *p = word;  //check that word has more than three characters
 	int size = strlen(word);
 	if(size < 3){                                                                
     strcpy(word, "");
@@ -101,18 +93,16 @@ int main(int argc, char *argv[]){
 		printf("usage: indexer <pagedir> <indexnm>\n");
 		exit(EXIT_FAILURE);
 	}
-	//long int num=atol(argv[1]);
-	//printf("%d",num);
 	hashtable_t *wordHash = hopen(100);
 	char* loadDir = (char*)malloc(100*sizeof(char));
 	char* saveDir = "../indexes/";
 
 	char* nameOfFile = (char*)malloc(100*sizeof(char));
-	strcpy(nameOfFile, argv[2]); // NEED TO : change 3 to 2
+	strcpy(nameOfFile, argv[2]); 
 
 	char fullSave[100] = {0};
 	sprintf(fullSave,"%s%s",saveDir,nameOfFile);
-	strcpy(loadDir, argv[1]); // NEED TO: change 2 to 1
+	strcpy(loadDir, argv[1]); 
 
 	int num = 1;
 	webpage_t* w;
@@ -128,10 +118,8 @@ int main(int argc, char *argv[]){
 		
 			NormalizeWord(currWord);        //normalize word
 			if(strcmp(currWord,"")){        //run if valid word
-				//printf("%s\n",currWord);
-				word_t *e=(word_t*)hsearch(wordHash, hashContainsWord, currWord, strlen(currWord));
-			//sees if hash contains word
-				
+			//check if hash contains word
+			word_t *e=(word_t*)hsearch(wordHash, hashContainsWord, currWord, strlen(currWord));	
 				if (e){  //if hash contains word
 					doc_t *d=(doc_t*)qsearch(e->docs, queueContainsDoc, id);  //checks if word queue contains doc
 					if(d!=NULL){           //if it does
@@ -169,17 +157,11 @@ int main(int argc, char *argv[]){
 					hput(wordHash, newWord, currWord, strlen(currWord)); // if we don't find it add that word to the hash
 				}		
 			}
-			//printf("w:%d",sizeof(webpage_t));
-			
 			free(currWord);
 		}
-		//free(webpage_getURL(w));
 		webpage_delete(w);
 		num+=1;
-	//free(currWord);
 	}
-	
-	//webpage_delete(w);
 	happly(wordHash, adderFunc); // calculate total_count
 	happly(wordHash, addObj);
 	hashFile = fopen(fullSave,"w");
@@ -191,8 +173,5 @@ int main(int argc, char *argv[]){
 	hclose(wordHash);
 	free(nameOfFile);
 	free(loadDir);
-	//fclose(f);
-	//printf("words:%d",total_count);
-	//printf("\nobjects:%d",total_obj);
 	return 0;
 }
